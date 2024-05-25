b679a47daea6defbf129614eafc3a46a2566d341
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2024-05-25 10:33:21 -0300

    init repositorie principal objects and paths

---
+++ tm1-constructor/utils/utils.py
@@ -0,0 +1,23 @@
+import json
+import sys, os
+
+CONFIG_PATH = r'\..\config\database_parameters.json'
+
+def getConnection():
+    with open(set_current_directory() + CONFIG_PATH) as op:
+        config = json.load(op)
+    connection = config['database_connection']
+    return connection
+
+def set_current_directory():
+    # determine if application is a script file or frozen exe
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.abspath(sys.executable)
+    elif __file__:
+        application_path = os.path.abspath(__file__)
+    directory = os.path.dirname(application_path)
+    # set current directory
+    os.chdir(directory)
+    return directory
+
+
