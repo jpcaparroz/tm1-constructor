8887607fd3af124aa72bf04a2713f1d501e08563
João Pedro Dias Caparroz <joao.caparroz@netpartners.com.br>
2024-05-28 18:08:30 -0300

    Element: Imports + function params

--- TM1Constructor/Constructor/ElementConstructor.py
+++ TM1Constructor/Constructor/ElementConstructor.py
@@ -1,3 +1,4 @@
+from typing import Union
 from TM1py import Element
 
 from TM1Constructor.Model import ElementModel
@@ -7,7 +8,7 @@ class ElementConstructor:
     def __init__(self) -> None:
         pass
     
-    def create_from_dict(self, element: ElementModel) -> Element:
+    def create(self, element: Union[ElementModel, dict]) -> Element:
         """Create an element object from a dict.
 
         Args:
@@ -16,6 +17,9 @@ class ElementConstructor:
         Returns:
             Element: A Element TM1py object.
         """
+        if not isinstance(element, ElementModel):
+            element = ElementModel(**element)
+        
         element_object = Element(
             name=element.Name,
             element_type=element.Type
