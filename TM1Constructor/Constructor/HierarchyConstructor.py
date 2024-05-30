dbcc849ad170f2b101952729de7cd0fe10937363
João Pedro Dias Caparroz <joao.caparroz@netpartners.com.br>
2024-05-30 18:59:57 -0300

    tests on hierarchy constructor

--- TM1Constructor/Constructor/HierarchyConstructor.py
+++ TM1Constructor/Constructor/HierarchyConstructor.py
@@ -1,10 +1,10 @@
-from typing import Union
+from typing import Union, List
 from TM1py import Hierarchy
 
 from TM1Constructor.Constructor.ElementConstructor import ElementConstructor
 from TM1Constructor.Constructor.EdgeConstructor import EdgeConstructor
 from TM1Constructor.Constructor.SubsetConstructor import SubsetConstructor
-from TM1Constructor.Model import HierarchyModel
+from TM1Constructor.Model.HierarchyModel import HierarchyModel
 
 class HierarchyConstructor:
     
@@ -16,7 +16,17 @@ class HierarchyConstructor:
         pass
     
     def create(self, dimension_name: str, hierarchy: Union[HierarchyModel, dict]) -> Hierarchy:
+        """Create an hierarchy object from a expected HierarchyModel.
 
+        Args:
+        -----
+            dimension_name (str): Dimension name of subset
+            hierarchy (Union[HierarchyModel, dict]): A HierarchyModel or a dict based on HierarchyModel
+
+        Returns:
+        --------
+            Hierarchy: A Hierarchy TM1py object.
+        """
         if not isinstance(hierarchy, HierarchyModel):
             hierarchy = HierarchyModel(**hierarchy)
         
@@ -24,7 +34,7 @@ class HierarchyConstructor:
             name=hierarchy.Name,
             dimension_name=dimension_name,
             elements=self.element.create_from_list(hierarchy.Elements),
-            edges=self.edge.create(hierarchy.Edges),
+            edges=self.edge.create_from_list(hierarchy.Edges),
             subsets=self.subset.create_from_list(
                 dimension_name=dimension_name,
                 hierarchy_name=hierarchy.Name,
@@ -32,3 +42,18 @@ class HierarchyConstructor:
         )
         
         return hierarchy_object
+    
+    def create_from_list(self, dimension_name: str, hierarchies: List[Union[HierarchyModel, dict]]) -> List[Hierarchy]:
+        """Create a list of hierarchy objects from a expected HierarchyModel.
+
+        Args:
+        -----
+            dimension_name (str): Dimension name of subset
+            hierarchies (List[Union[HierarchyModel, dict]]): A list of HierarchyModel or a dict based on HierarchyModel
+
+        Returns:
+        --------
+            List[Hierarchy]: List of Hierarchy TM1py object.
+        """
+        
+        return [self.create(dimension_name=dimension_name, hierarchy=hierarchy) for hierarchy in hierarchies]
