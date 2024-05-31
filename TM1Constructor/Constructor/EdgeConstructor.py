b81619a89122af96862106ff071b6dbdb754a8c3
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2024-05-30 23:02:34 -0300

    requirements update + tests

--- TM1Constructor/Constructor/EdgeConstructor.py
+++ TM1Constructor/Constructor/EdgeConstructor.py
@@ -9,17 +9,42 @@ class EdgeConstructor:
     def __init__(self) -> None:
         pass
 
-    def create_from_list(self, edges: List[Union[EdgeModel, dict]]) -> CaseAndSpaceInsensitiveTuplesDict:
-        treated_edges = []
-        for edge in edges:
-            if not isinstance(edge, EdgeModel):
-                treated_edges.append(EdgeModel(**edge))
+    def create(self, edge: Union[EdgeModel, dict]) -> CaseAndSpaceInsensitiveTuplesDict:
+        """Create an CaseAndSpaceInsensitiveTuplesDict object from a expected EdgeModel.
 
-        edge_objects = CaseAndSpaceInsensitiveTuplesDict(
+        Args:
+        -----
+            edge (Union[EdgeModel, dict]): A EdgeModel or a dict based on EdgeModel
+
+        Returns:
+        --------
+            CaseAndSpaceInsensitiveTuplesDict: A CaseAndSpaceInsensitiveTuplesDict TM1py object.
+        """
+        if not isinstance(edge, EdgeModel):
+            edge = EdgeModel(**edge)
+
+
+        edge_object = CaseAndSpaceInsensitiveTuplesDict(
             {
                 (edge.ParentName, edge.ComponentName): edge.Weight
-                for edge in treated_edges
             }
         )
 
+        return edge_object
+
+    def create_from_list(self, edges: List[Union[EdgeModel, dict]]) -> CaseAndSpaceInsensitiveTuplesDict:
+        """Create a CaseAndSpaceInsensitiveTuplesDict objects from a expected EdgeModel.
+
+        Args:
+        -----
+            edges (List[Union[HierarchyModel, dict]]): A list of EdgeModel or a dict based on EdgeModel
+
+        Returns:
+        --------
+            CaseAndSpaceInsensitiveTuplesDict: A CaseAndSpaceInsensitiveTuplesDict TM1py object.
+        """
+        edge_objects = {}
+        for edge in edges:
+            edge_objects.update(self.create(edge))
+            
         return edge_objects
