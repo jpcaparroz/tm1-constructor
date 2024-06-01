a1c40433fdb37698d33fb0c31dec318c4965257d
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2024-06-01 11:42:12 -0300

    testing dimension creator with attributes

--- TM1Constructor/Constructor/DimensionConstructor.py
+++ TM1Constructor/Constructor/DimensionConstructor.py
@@ -1,8 +1,10 @@
 from typing import Union, List
-from TM1py import Dimension
+from TM1py import TM1Service, Dimension
 
 from TM1Constructor.Model.DimensionModel import DimensionModel
+from TM1Constructor.Model.ElementAttributeModel import ElementAttributeModel
 from TM1Constructor.Constructor.HierarchyConstructor import HierarchyConstructor
+from TM1Constructor.Constructor.ElementAttributeConstructor import ElementAttributeConstructor
 
 class DimensionConstructor:
     
@@ -11,6 +13,7 @@ class DimensionConstructor:
     
     def __init__(self) -> None:
         self.hierarchy = HierarchyConstructor()
+        self.element_attribute = ElementAttributeConstructor()
 
         pass
     
@@ -25,8 +28,7 @@ class DimensionConstructor:
         --------
             Dimension: A Dimension TM1py object.
         """
-        if not isinstance(dimension, DimensionModel):
-            dimension = DimensionModel(**dimension)
+        dimension = self.__change_istance_if_dict(dimension=dimension)
         
         dimension_object = Dimension(
             name=dimension.Name,
@@ -48,3 +50,20 @@ class DimensionConstructor:
         """
         
         return [self.create(dimension=dimension) for dimension in dimensions]
+    
+    def create_attributes(self, tm1_service: TM1Service, dimension: Union[DimensionModel, dict]):
+        dimension = self.__change_istance_if_dict(dimension=dimension)
+        
+        for element_attribute in dimension.Attributes:
+            return tm1_service.elements.create_element_attribute(dimension_name=dimension.Name, 
+                                                                 hierarchy_name=dimension.Name, 
+                                                                 element_attribute=self.element_attribute.create(element_attribute=element_attribute))
+             
+    def __check_instance(self, dimension: Union[DimensionModel, dict]) -> bool:
+        return isinstance(dimension, DimensionModel)
+    
+    def __change_istance_if_dict(self, dimension: Union[DimensionModel, dict]) -> DimensionModel:
+        if self.__check_instance(dimension):
+            return dimension
+        else:
+            return DimensionModel(**dimension)
