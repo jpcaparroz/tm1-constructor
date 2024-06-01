8c3f0dee45b192472b8c4c96b2a252d38e27a13c
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2024-06-01 11:05:26 -0300

    element attributor constructor

---
+++ TM1Constructor/Constructor/ElementAttributeConstructor.py
@@ -0,0 +1,52 @@
+from typing import Union, List
+from TM1py import ElementAttribute
+
+from TM1Constructor.Model.ElementAttributeModel import ElementAttributeModel
+
+class ElementAttributeConstructor:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def create(self, element_attribute: Union[ElementAttributeModel, dict]) -> ElementAttribute:
+        """Create an element object from a dict.
+
+        Args:
+        -----
+            element_attribute (Union[ElementAttributeModel, dict]): A ElementAttributeModel or a dict based on ElementAttributeModel
+
+        Returns:
+        --------
+            ElementAttribute: A ElementAttribute TM1py object.
+        """
+        element_attribute = self.__change_istance_if_dict(element_attribute=element_attribute)
+        
+        element_attribute_object = ElementAttribute(
+            name=element_attribute.Name,
+            attribute_type=element_attribute.Type
+        )
+        
+        return element_attribute_object
+    
+    def create_from_list(self, element_attributes: List[Union[ElementAttributeModel, dict]]) -> List[ElementAttribute]:
+        """Create a list of element attribute objects from a expected ElementAttributeModel.
+
+        Args:
+        ----
+            element_attributes (List[Union[ElementAttributeModel, dict]]): A list of ElementAttributeModel or a dict based on ElementAttributeModel
+
+        Returns:
+        --------
+            List[ElementAttribute]: List of ElementAttribute TM1py object.
+        """
+        
+        return [self.create(element_attribute=element_attribute) for element_attribute in element_attributes]
+
+    def __check_instance(element_attribute: Union[ElementAttributeModel, dict]) -> bool:
+        return isinstance(element_attribute, ElementAttributeModel)
+    
+    def __change_istance_if_dict(self, element_attribute: Union[ElementAttributeModel, dict]) -> ElementAttributeModel:
+        if self.__check_instance(element_attribute):
+            return element_attribute
+        else:
+            return ElementAttributeModel(**element_attribute)
--- TM1Constructor/Constructor/__init__.py
+++ TM1Constructor/Constructor/__init__.py
@@ -3,4 +3,5 @@ from TM1Constructor.Constructor.DimensionConstructor import DimensionConstructor
 from TM1Constructor.Constructor.HierarchyConstructor import HierarchyConstructor
 from TM1Constructor.Constructor.EdgeConstructor import EdgeConstructor
 from TM1Constructor.Constructor.ElementConstructor import ElementConstructor
+from TM1Constructor.Constructor.ElementAttributeConstructor import ElementAttributeConstructor
 from TM1Constructor.Constructor.SubsetConstructor import SubsetConstructor
