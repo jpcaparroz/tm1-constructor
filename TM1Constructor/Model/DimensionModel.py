21675f03b3873a0216973ad2fad3be36015cd996
João Pedro Dias Caparroz <joao.caparroz@netpartners.com.br>
2024-06-02 15:52:07 -0300

    caption model

--- TM1Constructor/Model/DimensionModel.py
+++ TM1Constructor/Model/DimensionModel.py
@@ -3,8 +3,10 @@ from typing import List
 
 from TM1Constructor.Model.HierarchyModel import HierarchyModel
 from TM1Constructor.Model.ElementAttributeModel import ElementAttributeModel
+from TM1Constructor.Model.CaptionModel import CaptionModel
 
 class DimensionModel(BaseModel):
     Name: str
+    Caption: CaptionModel
     Attributes: List[ElementAttributeModel]
     Hierarchies: List[HierarchyModel]
--- TM1Constructor/Model/__init__.py
+++ TM1Constructor/Model/__init__.py
@@ -5,4 +5,5 @@ from TM1Constructor.Model.HierarchyModel import HierarchyModel
 from TM1Constructor.Model.EdgeModel import EdgeModel
 from TM1Constructor.Model.ElementModel import ElementModel
 from TM1Constructor.Model.ElementAttributeModel import ElementAttributeModel
+from TM1Constructor.Model.CaptionModel import CaptionModel
 from TM1Constructor.Model.SubsetModel import SubsetModel
